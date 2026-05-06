c375fc06e6a23ba2a2dad0a821af5316150fe76c
CPriceSouls <92562340+ayu5peecaptain@users.noreply.github.com>
2026-05-06 10:29:47 +0700

    Full Tech Git Repo

---
+++ C/.vscode/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h> // Run by stdio.h (header file)
+/*
+ * %d = integer
+ * %f = decimal
+ * %c = Single string
+ * %s = string with symbol such as ^$
+ * \n = new tap
+ * \t = indent
+ */
+
+// int main()
+// {
+//     // printf("Student Data \n");
+//     // printf("Name = %s\t%s","Thatchaphon", "Panyajamorn \n");
+//     // printf("Age = %d years old\n", 30-12);
+//     // printf("Gender = %c\n", 'M');
+//     // printf("GPA = %f \n",3.58);
+//     // printf("Address = %s\nTel = %s", "Hatyai", "0960761531");
+//     // return 0;
+// }
+
+int main() {
+
+
+}
